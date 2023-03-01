68c8578d1eb632a771c6e05ecaf94657ea281022
jariza-o <juan.ariza@gmail.com>
2023-03-01 20:12:04 +0100

    casi listo, falla al recibir y el make

--- cliente.c
+++ cliente.c
@@ -12,45 +12,52 @@
 
 #include "minitalk.h"
 
-void	send(int pid, char c)
+void	ft_send(int pid, char c)
 {
 	int	i;
 
 	i = 0;
 	while (i < 8)
 	{
-		if (c && (1 << i) != 0) 
+		if (c && (1 << i) != 0)
+		{
 			// (1 << c) mueve la posición del bit a la izquierda, y se mueve una posición sola porque el nº es binario. La primera vez que empieza esta en el final y al empezar el bucle se mueve una posición y ve el primer bit. CUando mueves ve solo de un bit a un bit, no ve los demás de la izq
 			// (1 << c) correcto: se deplaza hacia a la izquierda a  la posicion c (que es el contador), y en esa posicion escribe un 1			
 			kill(pid, SIGUSR1); //manda una señal al PID indicado cuando el bit que esta viendo es 1
+		}
 		else
+		{
 			kill(pid, SIGUSR2); //manda una señal al PID indicado cuando el bit que esta viendo es 0
-		else
+		}
 		usleep(50); //time slepp de 50 micro segundos
-	i++;
+		i++;
 	}
 }
 
-// void	check_signal(int i)
-// {
-// 	if (i == SIGUSR1)
-// 		ft_printf("The message has been sent correctly. \n");
-// }
+void	check_signal(int i) //PARA QUE SIRVE ??? ES PARA EN SA_HANDLER??
+{
+	if (i == SIGUSR1)
+		ft_printf("The message has been sent correctly. \n");
+}
 
 int	main(int argc, char **argv)
 {
 	int	pid;
 	int	n;
+	struct sigaction	sign;
 
+	n = 0;
 	if (argc == 3)
 	{
 		pid = ft_atoi(argv[1]); // paso el PID de char a int
+		sign.sa_handler = &check_signal; // sa_handler: dirección de la función manejadora de la señal, 
+		sigaction(SIGUSR1, &sign, NULL); // define la acción a realizar cuando se recibe una señal, es la versión POSIX de signal.
 		while (argv[2][n] != '\0')
 		{
-			send(pid, argv[2][n]);
+			ft_send(pid, argv[2][n]);
 			n++;
 		}
-		send(pid, argv[2][n]);
+		ft_send(pid, argv[2][n]);
 	}
 	else
 	{
--- servidor.c
+++ servidor.c
@@ -12,20 +12,21 @@
 
 #include "minitalk.h"
 
-void	ft_heandler(int signal, const struct sigaction *act, struct sigaction *oldact) // creo que hay que modificar las dos ultimas variables
+void	ft_handler(int signal, siginfo_t *act, void *oldact) // creo que hay que modificar las dos ultimas variables
 	// signal: señal a recibir
 	// act y oldact son las estructuras que definen el nuevo y viejo comportamiento para tratar la seña
 {
 	static unsigned char	byte;
 	static int	i;
 
+	(void)oldact; // necesita esto porque esta variable nose usa pero está debido a la estructura de sigaction
 	if (signal == SIGUSR1)
 		byte |= (1 << i);
 	//byte se inicializa la primera vez como 0, es decir, no hay nada. Si la seña recibida es 1, pone un 1 en la posición de i
 	i++;
 	if (i == 8) //significa que ya ha recibido toda la señal al completo
 	{
-		if (byte = '\0') // si es caracter nulo imprime un salto de linea porque termina lo recibido
+		if (byte == '\0') // si es caracter nulo imprime un salto de linea porque termina lo recibido
 		{
 			ft_printf("\n");
 			kill(act->si_pid, SIGUSR1); //no entiendo
@@ -50,19 +51,19 @@ int	main(void)
 	sign.sa_flags = SA_SIGINFO; 
 		// .sa_flags: directrices para el proceso cuando recibe la señal.
 		// SA_INFO: caundo se escribe SA_INFO se asume que la funcion manejadora está apuntada por sa_sigaction.
-	sigaddset(&sign.sa_mask, SIGURS1); 
+	sigaddset(&sign.sa_mask, SIGUSR1); 
 		// sigaddset: Añade una señal a un conjunto, poniendo a uno el bit correspondiente.
 		// sa_mask: para cadda señal, mascara de señales a bloquear cuando se ejecuta la función manejadora de esta señal,
 		// se añade la señal que está siendo atendida
 
 		// resumen: añade SIGURS1 a la lista .sa_mask, para cuando se ejecute ft_handler no pueda recibir nuevas señales de este tipo.
 
-	sigaddset(&sign.sa_mask, SIGURS2);
+	sigaddset(&sign.sa_mask, SIGUSR2); // sigaddset: añade una señal a un conjunto de señales
 	while (1)
 	{
-		sigaction(SIGURS1, &sign, NULL);
-		sigaction(SIGURS2, &sign, NULL);
-		pause();
+		sigaction(SIGUSR1, &sign, NULL); // define la acción a realizar cuando se recibe una señal, es la versión POSIX de signal.
+		sigaction(SIGUSR2, &sign, NULL);
+		pause(); // suspende al proceso solicitante hasta recibir una señal
 	}
 	return (0);
 }
